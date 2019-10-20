a754e66b5f214c096e14bca328dd7d120f565aa2
corganfuzz <gruizcavero@uh.edu>
2019-10-20 14:50:32 -0500

    greq done

---
+++ greq/greq.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+
+	"github.com/levigross/grequests"
+)
+
+func main() {
+
+	resp, err := grequests.Get("http://httpbin.org/get", nil)
+
+	if err != nil {
+		log.Fatalln("Unable to make request: ", err)
+	}
+
+	var returnData map[string]interface{}
+	resp.JSON(&returnData)
+	log.Println(returnData)
+	// log.Println(resp.String())
+}
